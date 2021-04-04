7c476e9a428d12f2111b3b00b0fec0cb17e6b3f4
Joel Bradshaw <cincodenada@gmail.com>
2021-04-04 11:13:43 -0700

    Just disable everything we can for now
    We'll reenable things once the rest is building, start small

--- Ports/OpenRCT2/package.sh
+++ Ports/OpenRCT2/package.sh
@@ -3,7 +3,16 @@ port="OpenRCT2"
 version="0.3.3"
 files="https://github.com/OpenRCT2/OpenRCT2/archive/refs/tags/v${version}.tar.gz OpenRCT2-${version}.tar.gz"
 depends="SDL2 openssl"
-configopts="-DCMAKE_TOOLCHAIN_FILE=$SERENITY_ROOT/Toolchain/CMakeToolchain.txt -DENABLE_SCRIPTING=OFF"
+configopts="-DCMAKE_TOOLCHAIN_FILE=$SERENITY_ROOT/Toolchain/CMakeToolchain.txt
+    -DENABLE_SCRIPTING=OFF
+    -DDISABLE_NETWORK=ON
+    -DDISABLE_HTTP=ON
+    -DDISABLE_GUI=ON
+    -DDISABLE_TTF=ON
+    -DDISABLE_DISCORD_RPC=ON
+    -DDISABLE_GOOGLE_BENCHMARK=ON
+    -DENABLE_LIGHTFX=OFF
+"
 workdir=${port}-${version}
 
 build() {
